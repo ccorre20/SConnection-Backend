3a937236a8ad0f293c8633a777ca774883064602
ccr185 <ccorre20@eafit.edu.co>
2015-10-10 08:34:10 -0500

    FC

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,15 +2,6 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     if params[:id]
-      if (@u = User.find_by_id(params[:id])) != nil
-        render json: @u
-      else
-        render :json => { :error => "gigante" }
-      end
-    else
-      @u = User.find(1)
-      render json: @u
-    end
   end
 
 end
