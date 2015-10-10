4e3f8f25ff51f99e9f198274d20de03076284f80
ccr185 <ccorre20@eafit.edu.co>
2015-10-10 10:55:26 -0500

    fix and prepare new model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :user_t, presence: true
-
+  
+  has_many :services, :class_name => 'Service', :foreign_key => 'user_id'
+  has_many :services, :class_name => 'Service', :foreign_key => 'provider_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010143943) do
+ActiveRecord::Schema.define(version: 20151010152836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "s_date"
+    t.integer  "user_id"
+    t.integer  "provider_id"
+    t.string   "s_status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
