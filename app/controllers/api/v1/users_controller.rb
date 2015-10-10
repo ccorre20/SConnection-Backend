a0b129890a8fe6620ea3ac3b9e63a4331c19116a
ccr185 <ccorre20@eafit.edu.co>
2015-10-10 09:06:54 -0500

    FC

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-    if params[:id]
+    if params[:login] && params[:login] == 'true' && params[:name] 
+      if (@u = User.find_by name: params[:name]) != nil
+        render json: @u , status: 200
+      else
+        render json: { error: "usuario inexistente"}, status: 404
+      end
+    else
+      render json: { error: "request incompleto" }, status: 400
+    end
   end
 
 end
