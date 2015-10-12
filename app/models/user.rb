25c69f97d2894f0a51f64b6b093faafdf6ea764c
ccr185 <ccorre20@eafit.edu.co>
2015-10-11 22:17:02 -0500

    fix location relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   has_many :services_as_user, :class_name => 'Service', :foreign_key => 'user_id'
   has_many :services_as_provider, :class_name => 'Service', :foreign_key => 'provider_id'
 
-  has_one :location, :class_name => 'Location', :foreign_key => 'user_id'
+  has_one :location
 end
