b5185afe52fea1bb7d9fec24a546c6bc1973ba32
ccr185 <ccorre20@eafit.edu.co>
2015-11-01 19:04:44 -0500

    add provider profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101232030) do
+ActiveRecord::Schema.define(version: 20151102000357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20151101232030) do
     t.string   "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "provider_profiles", force: :cascade do |t|
+    t.integer  "provider_id"
+    t.string   "description"
+    t.float    "rating"
+    t.string   "availability"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "service_statuses", force: :cascade do |t|
